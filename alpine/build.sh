5e913c7c6c953397cff299e89e87fc9d02eaeb3e
versus <versus.ua@gmail.com>
2021-02-17 14:53:12 +0200

    add Dokerfile for test tf-swither

--- alpine/build.sh
+++ alpine/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm  ../dist/terraform-switcher-alpine.tar.gz || true
 env ../version
-cd .. && docker build  -f alpine/Dockerfile  -t alpine-build .
+cd .. && docker build  -f alpine/Dockerfile.builder  -t alpine-build .
 cd dist && docker run --rm -iv${PWD}:/host-volume alpine-build  sh -s <<EOF
 chown -v $(id -u):$(id -g) /app/terraform-switcher
 cp -va /app/terraform-switcher  /host-volume/terraform-switcher-alpine
